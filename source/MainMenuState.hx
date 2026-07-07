cc1c83e4ee131c58c20b8cda56bea84b1e6332ca
wubboxcap <145715143+wubboxcap@users.noreply.github.com>
2026-07-07 11:17:37 -0400

    a bit of changes

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -130,7 +130,7 @@ class MainMenuState extends MusicBeatState
 
 		FlxG.camera.follow(camFollowPos, null, 1);
 
-		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Alight Engine v" + alightEngineVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Alight Engine v" + alightEngineVersion + " (Modified Psych Engine v0.6.3)", 12);
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
--- source/OutdatedState.hx
+++ source/OutdatedState.hx
@@ -25,7 +25,7 @@ class OutdatedState extends MusicBeatState
 
 		warnText = new FlxText(0, 0, FlxG.width,
 			"Sup bro, looks like you're running an   \n
-			outdated version of Psych Engine (" + MainMenuState.psychEngineVersion + "),\n
+			outdated version of Alight Engine (" + MainMenuState.alightEngineVersion + "),\n
 			please update to " + TitleState.updateVersion + "!\n
 			Press ESCAPE to proceed anyway.\n
 			\n
