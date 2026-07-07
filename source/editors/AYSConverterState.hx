ba14ddbdbe2a424a869299e48a6a22f230d4490f
wubboxcap <145715143+wubboxcap@users.noreply.github.com>
2026-07-06 22:57:35 -0400

    Init - 0.0.1

---
+++ source/editors/AYSConverterState.hx
@@ -0,0 +1,262 @@
+package editors;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.util.FlxAxes;
+import flixel.system.FlxSound;
+import flixel.addons.ui.FlxInputText;
+import flixel.addons.ui.FlxUI9SliceSprite;
+import flixel.addons.ui.FlxUI;
+import flixel.addons.ui.FlxUICheckBox;
+import flixel.addons.ui.FlxUIInputText;
+import flixel.addons.ui.FlxUINumericStepper;
+import flixel.addons.ui.FlxUITabMenu;
+import flixel.ui.FlxButton;
+import openfl.net.FileReference;
+import openfl.events.Event;
+import openfl.events.IOErrorEvent;
+import flash.net.FileFilter;
+import Section.SwagSection;
+import haxe.Json;
+import haxe.format.JsonParser;
+import DialogueBoxPsych;
+import lime.system.Clipboard;
+import Alphabet;
+#if sys
+import sys.io.File;
+#end
+
+using StringTools;
+typedef SwagSong =
+{
+	var song:String;
+	var notes:Array<SwagSection>;
+	var events:Array<Dynamic>;
+	var bpm:Float;
+	var needsVoices:Bool;
+	var speed:Float;
+
+	var player1:String;
+	var player2:String;
+	var gfVersion:String;
+	var stage:String;
+
+	var arrowSkin:String;
+	var splashSkin:String;
+	var validScore:Bool;
+}
+typedef Chart = {
+    var song:SwagSong;
+}
+class AYSConverterState extends MusicBeatState
+{
+	var box:FlxSprite;
+	var daText:TypedAlphabet;
+
+	var selectedText:FlxText;
+	var animText:FlxText;
+
+	var defaultLine:DialogueLine;
+	var dialogueFile:DialogueFile = null;
+
+	override function create() {
+		#if desktop
+		// Updating Discord Rich Presence
+		DiscordClient.changePresence("In the AYS Converter", null);
+		#end
+		FlxG.camera.bgColor = FlxColor.BLACK;
+		var bg:FlxSprite = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
+		bg.scrollFactor.set();
+		bg.color = 0xFF353535;
+		add(bg);
+		var addLineText:FlxText = new FlxText(0, 250, -1, 'Select a chart.');
+		addLineText.setFormat(Paths.font("vcr.ttf"), Std.int(FlxG.width/24), FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		addLineText.scrollFactor.set();
+		addLineText.screenCenter(FlxAxes.X);
+		add(addLineText);
+		var loadButton:FlxButton = new FlxButton(0, 550, "Load Chart", function() {
+			loadChart();
+		});
+		loadButton.scale.set(2, 2);
+		loadButton.updateHitbox();
+		loadButton.label.scale.set(2, 2);
+		loadButton.label.updateHitbox();
+		loadButton.screenCenter(FlxAxes.X);
+		add(loadButton);
+		persistentUpdate = persistentDraw = true;
+		FlxG.camera.bgColor = FlxColor.fromHSL(0, 0, 0.5);
+		FlxG.mouse.visible = true;
+		super.create();
+	}
+
+	private static var DEFAULT_TEXT:String = "coolswag";
+	private static var DEFAULT_SPEED:Float = 0.05;
+	private static var DEFAULT_BUBBLETYPE:String = "normal";
+
+	var curSelected:Int = 0;
+	var curAnim:Int = 0;
+	var blockPressWhileTypingOn:Array<FlxUIInputText> = [];
+	var transitioning:Bool = false;
+	override function update(elapsed:Float) {
+		if(transitioning) {
+			super.update(elapsed);
+			return;
+		}
+		if(FlxG.keys.justPressed.ESCAPE) {
+			MusicBeatState.switchState(new editors.MasterEditorMenu());
+			FlxG.sound.playMusic(Paths.music('freakyMenu'), 1);
+			transitioning = true;
+		}
+		super.update(elapsed);
+	}
+
+	var _file:FileReference = null;
+	function loadDialogue() {
+		var jsonFilter:FileFilter = new FileFilter('JSON', 'json');
+		_file = new FileReference();
+		_file.addEventListener(Event.SELECT, onLoadComplete);
+		_file.addEventListener(Event.CANCEL, onLoadCancel);
+		_file.addEventListener(IOErrorEvent.IO_ERROR, onLoadError);
+		_file.browse([jsonFilter]);
+	}
+	function loadChart() {
+		var jsonFilter:FileFilter = new FileFilter('JSON', 'json');
+		_file = new FileReference();
+		_file.addEventListener(Event.SELECT, onLoadComplete);
+		_file.addEventListener(Event.CANCEL, onLoadCancel);
+		_file.addEventListener(IOErrorEvent.IO_ERROR, onLoadError);
+		_file.browse([jsonFilter]);
+	}
+
+	function onLoadComplete(_):Void
+	{
+		_file.removeEventListener(Event.SELECT, onLoadComplete);
+		_file.removeEventListener(Event.CANCEL, onLoadCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onLoadError);
+
+		#if sys
+		var fullPath:String = null;
+		@:privateAccess
+		if(_file.__path != null) fullPath = _file.__path;
+
+		if(fullPath != null) {
+			var rawJson:String = File.getContent(fullPath);
+
+			if(rawJson != null) {
+				var loadedSong = Json.parse(rawJson);
+				if(loadedSong.song != null) //Make sure it's really a song json
+				{
+					var chart:Chart = loadedSong;
+					var song = loadedSong.song;
+					for (note in song.notes) {
+						var sectionNotes = note.sectionNotes;
+						if (sectionNotes == []) {
+							// skip
+						}
+					}
+				}
+				var loadedDialog:DialogueFile = cast Json.parse(rawJson);
+				if(loadedDialog.dialogue != null && loadedDialog.dialogue.length > 0) //Make sure it's really a dialogue file
+				{
+					var cutName:String = _file.name.substr(0, _file.name.length - 5);
+					trace("Successfully loaded file: " + cutName);
+					dialogueFile = loadedDialog;
+					_file = null;
+					return;
+				}
+			}
+		}
+		_file = null;
+		#else
+		trace("File couldn't be loaded! You aren't on Desktop, are you?");
+		#end
+	}
+
+	/**
+		* Called when the save file dialog is cancelled.
+		*/
+	function onLoadCancel(_):Void
+	{
+		_file.removeEventListener(Event.SELECT, onLoadComplete);
+		_file.removeEventListener(Event.CANCEL, onLoadCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onLoadError);
+		_file = null;
+		trace("Cancelled file loading.");
+	}
+
+	/**
+		* Called if there is an error while saving the gameplay recording.
+		*/
+	function onLoadError(_):Void
+	{
+		_file.removeEventListener(Event.SELECT, onLoadComplete);
+		_file.removeEventListener(Event.CANCEL, onLoadCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onLoadError);
+		_file = null;
+		trace("Problem loading file");
+	}
+
+	function saveDialogue() {
+		var data:String = Json.stringify(dialogueFile, "\t");
+		if (data.length > 0)
+		{
+			_file = new FileReference();
+			_file.addEventListener(Event.COMPLETE, onSaveComplete);
+			_file.addEventListener(Event.CANCEL, onSaveCancel);
+			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+			_file.save(data, "dialogue.json");
+		}
+	}
+	function saveChart() {
+		var data:String = Json.stringify(dialogueFile, "\t");
+		if (data.length > 0)
+		{
+			_file = new FileReference();
+			_file.addEventListener(Event.COMPLETE, onSaveComplete);
+			_file.addEventListener(Event.CANCEL, onSaveCancel);
+			_file.addEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+			_file.save(data, "Chart.txt");
+		}
+	}
+
+	function onSaveComplete(_):Void
+	{
+		_file.removeEventListener(Event.COMPLETE, onSaveComplete);
+		_file.removeEventListener(Event.CANCEL, onSaveCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+		_file = null;
+		FlxG.log.notice("Successfully saved file.");
+	}
+
+	/**
+		* Called when the save file dialog is cancelled.
+		*/
+	function onSaveCancel(_):Void
+	{
+		_file.removeEventListener(Event.COMPLETE, onSaveComplete);
+		_file.removeEventListener(Event.CANCEL, onSaveCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+		_file = null;
+	}
+
+	/**
+		* Called if there is an error while saving the gameplay recording.
+		*/
+	function onSaveError(_):Void
+	{
+		_file.removeEventListener(Event.COMPLETE, onSaveComplete);
+		_file.removeEventListener(Event.CANCEL, onSaveCancel);
+		_file.removeEventListener(IOErrorEvent.IO_ERROR, onSaveError);
+		_file = null;
+		FlxG.log.error("Problem saving file");
+	}
+}
